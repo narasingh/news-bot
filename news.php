2a4239b0a4da507c4217020083f54b21292865f3
narasingh <narasingh.mishra@target.com>
2017-03-31 10:55:55 +0530

    implemented google news api

--- news.php
+++ news.php
@@ -4,37 +4,31 @@ header("Access-Control-Allow-Origin: *");
   //$entityBody = file_get_contents('php://input');
   // Method: POST, PUT, GET etc
 // Data: array("param" => "value") ==> index.php?param=value
+  function curl_get_contents($url)
+  {
+    $ch = curl_init();
+    $timeout = 5;
 
-function testLangID() {
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
 
-     $siteurl = 'https://newsapi.org/v1/articles?source=google-news&sortBy=top&apiKey=17079e8f3fb24e55842030ff5783ef53';
-     $curl = curl_init();
-     $headers = array(
-    'Accept: application/json',
-    'Content-Type: application/json');
+    $data = curl_exec($ch);
 
-    //$ciroot = array("contentItems" => $ciarr);
-    //$data_str = json_encode($ciroot);
+    curl_close($ch);
 
-     curl_setopt($curl, CURLOPT_URL, $siteurl);
-     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-     curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "GET");
-     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-     curl_setopt($ch, CURLOPT_HEADER, 0);
-     curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-     curl_setopt($curl, CURLOPT_USERPWD, "........:........");
+    return json_decode($data);
+  }
 
-     $result = curl_exec($curl);
+$url="https://newsapi.org/v1/articles?source=google-news&sortBy=top&apiKey=17079e8f3fb24e55842030ff5783ef53";
+$contents = curl_get_contents($url);
+$randomIndex = array_rand($contents->articles);
 
-     curl_close($curl);
-
-     $decoded = json_decode($result, true);
-
-     return $result;
-}
 $response = array(
   'color' => 'green',
-  'message' => 'It\'s going to be sunny tomorrow!',
+  'message' => addslashes($contents->articles[$randomIndex]->title),
   'notify' => false,
   'message_format' => 'text'
 );
