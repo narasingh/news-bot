d0d763056e9fdc8698b4c41ae4e0cc20db055be8
narasingh <narasingh.mishra@target.com>
2017-06-16 11:38:48 +0530

    test bot

--- bot.php
+++ bot.php
@@ -1,4 +1,6 @@
 <?php
+header('content-type: application/json; charset=utf-8');
+header("Access-Control-Allow-Origin: *");
 $postdata = json_decode(file_get_contents("php://input"), true);
 $action = '';
 $data = $postdata['item'];
