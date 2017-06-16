209b000d454a3ca8ba2449ea53103a3916536705
narasingh <narasingh.mishra@target.com>
2017-06-16 12:13:06 +0530

    initial commit

--- bot.php
+++ bot.php
@@ -27,58 +27,25 @@ function afterInsert() {
   global $userId;
 
   $description = $userName. ' '. ' has submitted his timesheet';
-
-  $response = array(
-    "color" => "green",
-    "style" => "application",
-    "format" => "medium",
-    "id" => $userId,
-    "title" => "Timely",
-    "description" => $description,
-    "icon" =>  array(
-      "url" => "http://bit.ly/1S9Z5dF"
-    ),
-    "attributes" => array(
-      array(
-        "label" => "first project",
-        "value" => array(
-          "label" => "description and hours"
-        )
-      ),
-      array(
-        "label" => "second project",
-        "value" => array(
-          "icon" => array(
-            "url" => "http://bit.ly/1S9Z5dF"
-          ),
-          "label" => "description and hours",
-          "style" => "lozenge-complete"
-        )
-      )
-    )  
-  );
-
-/*
   $response = array(
     "color" => "green",
     "card" => array(
-      "style" => "link",
+      "style" => "media",
       "url" => 'http://i0.kym-cdn.com/photos/images/newsfeed/000/131/786/tumblr_ljkeuyjp1a1qafrh6.gif',
-      "id" => "fee4d9a3-685d-4cbd-abaa-c8850d9b1960",
-      "title" => 'TIMELY',
-      "description" => $description,
-      "date" => time(),
+      "id" => $userId,
+      "title" => 'Timely card layout',
+      "description" => array(
+        "value" => $description,
+        "format" => "text"
+      ),
       "thumbnail" => array(
         "url" => 'https://c.martech.zone/wp-content/uploads/2010/06/example-logo.png',
         "url@2x" => 'https://c.martech.zone/wp-content/uploads/2010/06/example-logo.png',
         "width" => 1193,
         "height" => 564
       )
-    ),
-    'message' => "test",
-    "message_format" => 'html'
+    )
   );
-  */
 
   return $response;
 }
