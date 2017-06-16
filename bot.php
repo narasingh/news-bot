5e3b0bb5383cb2a15088a4df4614d688c3dc6312
narasingh <narasingh.mishra@target.com>
2017-06-16 12:07:26 +0530

    initial commit

--- bot.php
+++ bot.php
@@ -6,6 +6,7 @@ $action = '';
 $data = $postdata['item'];
 $message = $data['message']['message'];
 $userName = $data['message']['from']['name'];
+$userId = $data['message']['id'];
 
 function getHelp() {
   $usages = 'Usage: \n';
@@ -23,10 +24,41 @@ function getHelp() {
 function afterInsert() {
 
   global $userName;
+  global $userId;
 
   $description = $userName. ' '. ' has submitted his timesheet';
 
+  $response = array(
+    "color" => "green",
+    "style" => "application",
+    "format" => "medium",
+    "id" => $userId,
+    "title" => "Timely",
+    "description" => $description,
+    "icon" =>  array(
+      "url" => "http://bit.ly/1S9Z5dF"
+    ),
+    "attributes" => array(
+      array(
+        "label" => "first project",
+        "value" => array(
+          "label" => "description and hours"
+        )
+      ),
+      array(
+        "label" => "second project",
+        "value" => array(
+          "icon" => array(
+            "url" => "http://bit.ly/1S9Z5dF"
+          ),
+          "label" => "description and hours",
+          "style" => "lozenge-complete"
+        )
+      )
+    )  
+  );
 
+/*
   $response = array(
     "color" => "green",
     "card" => array(
@@ -34,10 +66,7 @@ function afterInsert() {
       "url" => 'http://i0.kym-cdn.com/photos/images/newsfeed/000/131/786/tumblr_ljkeuyjp1a1qafrh6.gif',
       "id" => "fee4d9a3-685d-4cbd-abaa-c8850d9b1960",
       "title" => 'TIMELY',
-      "description" => array(
-        'format' => 'html',
-        'value' => $description
-      ),
+      "description" => $description,
       "date" => time(),
       "thumbnail" => array(
         "url" => 'https://c.martech.zone/wp-content/uploads/2010/06/example-logo.png',
@@ -45,8 +74,11 @@ function afterInsert() {
         "width" => 1193,
         "height" => 564
       )
-    )
+    ),
+    'message' => "test",
+    "message_format" => 'html'
   );
+  */
 
   return $response;
 }
